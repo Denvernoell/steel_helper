3b4edcfdc2beabf8b8e45fc7f5ca2ff2ef8edc17
denvernoell <denvernoell@gmail.com>
2021-02-25 23:17:51 -0800

    Bump version: 0.1.9 → 0.1.10

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'steel-helper'
 year = '2021'
 author = 'Denver Noell'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.9'
+version = release = '0.1.10'
 
 pygments_style = 'trac'
 templates_path = ['.']
